7c9611107c48330c505b64657e80f8add2a541a2
Lakshya_059 <lakshyakantiwal@gmail.com>
2025-04-17 02:02:16 +0530

    login and signup for all roles are completed

--- server/src/utils/cloudinary.js
+++ server/src/utils/cloudinary.js
@@ -1,4 +1,4 @@
-import {v2 as cloudinary } from "cloudinary";
+import { v2 as cloudinary } from "cloudinary";
 import dotenv from "dotenv";
 dotenv.config({});
 
@@ -13,10 +13,9 @@ export const uploadMedia = async (file) => {
         const uploadResponse = await cloudinary.uploader.upload(file, {
             resource_type: "auto"
         });
-
         return uploadResponse;
     } catch (error) {
-        console.log(error);
+        throw new Error(error.message); // Throwing error to be handled by the calling function
     }
 }
 
