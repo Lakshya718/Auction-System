2c8c78553d6b4d4dffb2052194474a2ee522b166
Lakshya_059 <lakshyakantiwal@gmail.com>
2025-08-07 15:03:50 +0530

    try 6

--- client/api/axios.js
+++ client/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://auction-system-deploy.onrender.com/api',
+  baseURL: 'https://auction-system-deploy.onrender.com/api',
   headers: { 'Content-Type': 'application/json' },
   withCredentials: true,
 });
